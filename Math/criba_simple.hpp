18d957dbf57697854b8fd568e63df27595a37f10
JorgeSlime <jt603691@gmail.com>
2025-02-09 23:10:57 -0400

    changed criba simple

--- Math/criba_simple.hpp
+++ Math/criba_simple.hpp
@@ -1,17 +1,21 @@
-const i32 N = 1e6 + 7; 
+constexpr i32 N = 1e6 + 7; 
+const size_t TS = (N / std::log(N)) * 2;
 std::vector<i32> primes; 
-std::array<bool,N> vis;
+std::bitset<N> used;
 void criba(){
-    vis[0] = vis[1] = true; 
-    const i32 R = (i32)std::sqrt(N); 
-    for(i32 i = 2; i<=R;i++){
-        if(!vis[i]){
-            for(i64 j = i*i;j<=N;j+=i){
-                vis[j] = true;
+    primes.reserve(TS);
+    used[0] = used[1] = true; 
+    const i32 R = i32(std::sqrt(N)); 
+    for(i32 i{2}; i<=R;i++){
+        if(not used[i]){
+            for(i64 j{i * i};j<=N;j+=i){
+                used[j] = true;
             }
         }
     }
-    for(auto i = 0;i<N;i++){
-        if(!vis[i]) primes.emplace_back(i);
+    for(i32 i{2};i<N;i++){
+        if(not used[i]) 
+            primes.emplace_back(i);
     }
 }
+
